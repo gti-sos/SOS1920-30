38003906b612c9c1a72eebb8007387e7a40bcbad
RAW <raw_el_estudiante@hotmail.com>
2020-03-21 18:52:12 +0000

    updatinf

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require("express");
 
 var app = express();
 
+var port = process.env.PORT || 80;
+app.use("/",express.static("./public"));
 app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
 
-app.listen(80);
+app.listen(80,() => {
+	console.log("Server ready");
+					});
 
-console.log("server ready");
+console.log("Starting server...");
