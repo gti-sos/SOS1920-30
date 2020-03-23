3237d27fa8f466f40c302acd08015373cea20645
RAW <raw_el_estudiante@hotmail.com>
2020-03-23 18:32:11 +0000

    updating

--- index.js
+++ index.js
@@ -27,14 +27,70 @@ var contacts = [
 
 const BASE_API_URL = "/api/v1";
 
+// GET CONTACTS
+
 app.get(BASE_API_URL+"/contacts", (req,res) =>{
 	res.send(JSON.stringify(contacts,null,2));
+	console.log("Data sent:"+JSON.stringify(contacts,null,2));
 });
 
+
+// POST CONTACTS
+
 app.post(BASE_API_URL+"/contacts",(req,res) =>{
-	contacts.push(req.body); 
-	res.sendStatus(201,"CREATED");
+	
+	var newContact = req.body;
+	
+	if((newContact == "") || (newContact.name == null)){
+		res.sendStatus(400,"BAD REQUEST");
+	} else {
+		contacts.push(newContact); 	
+		res.sendStatus(201,"CREATED");
+	}
 });
+
+// DELETE CONTACTS
+
+// GET CONTACT/XXX
+
+app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	
+	var name = req.params.name;
+	
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name == name);
+	});
+	
+	
+	if(filteredContacts.length >= 1){
+		res.send(filteredContacts[0]);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND");
+	}
+});
+
+// DELETE CONTACT/XXX
+
+app.delete(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	
+	var name = req.params.name;
+	
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name != name);
+	});
+	
+	
+	if(filteredContacts.length < contacts.length){
+		contacts = filteredContacts;
+		res.sendStatus(200);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND");
+	}
+	
+	
+});
+
+
 
 app.listen(port,() => {
 	console.log("Server ready");
