92fc80f3c557a890bc539a3a1fe15a7354d39c56
RAW <raw_el_estudiante@hotmail.com>
2020-03-23 19:44:54 +0000

    puting real parametrers

--- index.js
+++ index.js
@@ -14,84 +14,120 @@ app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
 
-var contacts = [
-	{ 
-		name: "peter",
-		phone: 123456	
+var indice_de_masa_corporal = [
+	{
+		place: "Alemania",
+		indice_de_masa_corporal: 25.32
+		
 	},
-	{ 
-		name: "pablo",
-		phone: 789456	
+	{
+		place: "Polonia",
+		indice_de_masa_corporal: 23.21
+		
+	},
+	{
+		place: "Hungría",
+		indice_de_masa_corporal: 24.45
+		
 	}
 ];
 
 const BASE_API_URL = "/api/v1";
 
-// GET CONTACTS
+//GET CONTACTS
 
-app.get(BASE_API_URL+"/contacts", (req,res) =>{
-	res.send(JSON.stringify(contacts,null,2));
-	console.log("Data sent:"+JSON.stringify(contacts,null,2));
+app.get(BASE_API_URL+"/indice_de_masa_corporal",(req,res) => {
+	res.send(JSON.stringify(indice_de_masa_corporal, null, 2));
+	console.log("Data sent:"+JSON.stringify(indice_de_masa_corporal,null,2));
 });
 
+//POST CONTACTS
 
-// POST CONTACTS
-
-app.post(BASE_API_URL+"/contacts",(req,res) =>{
-	
-	var newContact = req.body;
-	
-	if((newContact == "") || (newContact.name == null)){
-		res.sendStatus(400,"BAD REQUEST");
-	} else {
-		contacts.push(newContact); 	
-		res.sendStatus(201,"CREATED");
+app.post(BASE_API_URL+"/indice_de_masa_corporal",(req,res) => {
+	var newindice_de_masa_corporal = req.body;
+	if((newindice_de_masa_corporal == "") || (newindice_de_masa_corporal.place == null)){
+			res.sendStatus(400, "BAD REQUEST(no name provided)");
+	}
+	else{
+		indice_de_masa_corporal.push(newindice_de_masa_corporal);
+		res.sendStatus(201, "CREATED");
 	}
 });
 
-// DELETE CONTACTS
 
-// GET CONTACT/XXX
 
-app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+
+
+//GET CONTACT/XXX
+app.get(BASE_API_URL+"/indice_de_masa_corporal/:place",(req,res) => {
+	var place = req.params.place;
+	var filteredindice_de_masa_corporal = indice_de_masa_corporal.filter((c) =>{
+		return (c.place == place);
+	});
+	if(filteredindice_de_masa_corporal.length >= 1){
+		res.send(filteredindice_de_masa_corporal[0]);
+	}else{
+		res.sendStatus(404, "CONTACT NOT FOUND");
+	}
+		
 	
-	var name = req.params.name;
 	
-	var filteredContacts = contacts.filter((c) => {
-		return (c.name == name);
+//DELETE CONTACTS
+	app.delete(BASE_API_URL+"/indice_de_masa_corporal",(req,res) => {
+		if(indice_de_masa_corporal.length == 0){
+			res.sendStatus(404, "masa corporal NOT FOUND");
+		}else{
+			indice_de_masa_corporal = "";
+			res.sendStatus(200,"OK")
+		}
 	});
 	
 	
-	if(filteredContacts.length >= 1){
-		res.send(filteredContacts[0]);
-	}else{
-		res.sendStatus(404,"CONTACT NOT FOUND");
-	}
 });
-
-// DELETE CONTACT/XXX
-
-app.delete(BASE_API_URL+"/contacts/:name", (req,res)=>{
+//PUT CONTACT/XXX
+app.put(BASE_API_URL+"/indice_de_masa_corporal/:place",(req,res) => {
+	var place = req.params.place;
+	if(!place){
+		res.sendStatus(404);
+	}
+	else{
+		if(place == ""){
+			res.sendStatus(400, "PLACE DOES NOT EXIST")
+		}else{
+			place == req.body.place;
+			res.sendStatus(200, "PLACE PUT OK")
+		}
+	}
 	
-	var name = req.params.name;
 	
-	var filteredContacts = contacts.filter((c) => {
-		return (c.name != name);
+});
+//DELETE CONTACT/XXX
+
+app.delete(BASE_API_URL+"/indice_de_masa_corporal/:place",(req,res) => {
+	var place = req.params.place;
+	var filteredindice_de_masa_corporal = indice_de_masa_corporal.filter((c) =>{
+		return (c.place != place);
 	});
-	
-	
-	if(filteredContacts.length < contacts.length){
-		contacts = filteredContacts;
-		res.sendStatus(200);
+	if(filteredindice_de_masa_corporal.length < indice_de_masa_corporal.length){
+		indice_de_masa_corporal = filteredindice_de_masa_corporal;
+		res.sendStatus(200, "OK");
+		
 	}else{
-		res.sendStatus(404,"CONTACT NOT FOUND");
+		res.sendStatus(404, "masa corporal PLACE NOT FOUND");
 	}
-	
-	
 });
 
+//POST CONTACT/XXX
 
+app.post(BASE_API_URL+"/indice_de_masa_corporal/:place",(req,res) => {
+	
+		res.sendStatus(405, "METHOD NOT ALLOWED");
+	});
 
+//PUT CONTACT
+app.put(BASE_API_URL+"/indice_de_masa_corporal",(req,res) => {
+	res.sendStatus(405, "METHOD NOT ALLOWED");
+});
 app.listen(port,() => {
 	console.log("Server ready");
 					});
