d4f53e5070ed9d683ba5f7d14b2633930f2c0e51
rawojeda <57014398+rawojeda@users.noreply.github.com>
2020-04-11 19:25:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const imcapi = require(path.join(__dirname,"imcapi"));
+
+const port = process.env.PORT || 80;
+
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use("/",express.static("./public"));
+
+
+
+imcapi(app);
+
+app.listen(port,() => {
+	console.log("Server ready");
+					});
+
+console.log("Starting server...");
