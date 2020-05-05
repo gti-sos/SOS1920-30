51ff9cb61b29c4eeeadce7f36d576e058d324c08
Fran <fran@gmail.com>
2020-05-05 19:37:40 +0200

    finish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const back = require("./src/back/sugarconsumeAPI");
+const back2 = require("./src/back/imcAPI");
 const bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,7 @@ var port = process.env.PORT || 12345;
 
 app.use("/",express.static("./public"));
 back(app);
+back2(app);
 
 app.listen(port, () => {
 	console.log("Server ready");
