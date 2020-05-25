8da6e1dea854bb0e795a87fec70add6961438db0
Fran <fran@gmail.com>
2020-05-25 12:18:36 +0200

    access origin allow

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const backv1 = require("./src/back/sugarconsumeAPI/v1");
 
 const back2 = require("./src/back/imcAPI");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 var app = express();
 app.use(bodyParser.json());
+
+
+app.use(cors())
+
 
 
 var port = process.env.PORT || 12345;
